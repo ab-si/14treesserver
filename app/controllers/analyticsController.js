482346f5052d6e65893bed193bfcfd8e52490820
abhishek.singh <abhishek.singh@arista.com>
2021-05-30 13:45:29 +0530

    Added API to fetch boundaries

--- app/controllers/analyticsController.js
+++ app/controllers/analyticsController.js
@@ -14,3 +14,19 @@ module.exports.getTotalTree = async(req,res) => {
       res.status(error).send(errorMessage);
     }
 }
+
+module.exports.getLocBoundary = async(req, res) => {
+  const query = 'Select * from plot';
+  const boundaryQuery = 'Select index, lat, lng from plot_boundary where plot_id=$1'
+  try {
+    const {rows} = await dbQuery.query(query);
+    for (let plot in rows) {
+      let result = await dbQuery.query(boundaryQuery, [rows[plot].plot_id])
+      rows[plot].plot_boundary = result.rows;
+    }
+    res.status(status.success).send(rows)
+  } catch( error) {
+    errorMessage.error = 'An error Occured';
+    res.status(error).send(errorMessage);
+  }
+}
--- app/routes/analyticsRoute.js
+++ app/routes/analyticsRoute.js
@@ -3,5 +3,6 @@ const routes = require('express').Router();
 const info = require('../controllers/analyticsController');
 
 routes.get('/totaltree', info.getTotalTree);
+routes.get('/getSites', info.getLocBoundary)
 
 module.exports = routes;
